f1af26674cd36e3275b4177ab74e256a826252aa
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:19:02 +0100

    1st attempt

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+  $months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+?>
+<?php
+  $months = array();
+  $months [0] = 'Janvier';
+  $months [1] = 'Février';
+  $months [2] = 'Mars';
+  $months [3] = 'Avril';
+  $months [4] = 'Mai';
+  $months [5] = 'Juin';
+  $months [6] = 'Juillet';
+  $months [7] = 'Août';
+  $months [8] = 'Septembre';
+  $months [9] = 'Octobre';
+  $months [10] = 'Novembre';
+  $months [11] = 'Décembre';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Partie 3 Exercice 1 : Les tableaux</title>
+  </head>
+  <body>
+
+  </body>
+</html>
