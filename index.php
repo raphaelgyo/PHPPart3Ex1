c02be6a64132cd7fff410ba88e25d8583346d248
raphaelgyo <raphael.granizo@gmail.com>
2019-12-05 09:39:37 +0100

    correction

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
+// la fonction php "array" permet de créer un tableau
   $months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
 ?>
+<!-- peut également s'écrire de la manière suivante -->
 <?php
   $months = array();
-  $months [0] = 'Janvier';
-  $months [1] = 'Février';
-  $months [2] = 'Mars';
-  $months [3] = 'Avril';
-  $months [4] = 'Mai';
-  $months [5] = 'Juin';
-  $months [6] = 'Juillet';
-  $months [7] = 'Août';
-  $months [8] = 'Septembre';
-  $months [9] = 'Octobre';
-  $months [10] = 'Novembre';
-  $months [11] = 'Décembre';
+  $months[0] = 'Janvier';
+  $months[1] = 'Février';
+  $months[2] = 'Mars';
+  $months[3] = 'Avril';
+  $months[4] = 'Mai';
+  $months[5] = 'Juin';
+  $months[6] = 'Juillet';
+  $months[7] = 'Août';
+  $months[8] = 'Septembre';
+  $months[9] = 'Octobre';
+  $months[10] = 'Novembre';
+  $months[11] = 'Décembre';
+?>
+<?php
+  $days = array();
+  $days[0] = 'Lundi';
+  $days[1] = 'Mardi';
+  $days[2] = 'Mercredi';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>PHP Partie 3 Exercice 1 : Les tableaux</title>
   </head>
   <body>
